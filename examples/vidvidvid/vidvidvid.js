be0d7e7599d6896e80c6850414df46efeffc222c
=Abram Stern (aphid) <aphid@ucsc.edu>
2026-05-31 09:48:02 -0700

    wrapping up!

--- examples/vidvidvid/vidvidvid.js
+++ examples/vidvidvid/vidvidvid.js
@@ -2,8 +2,9 @@ let Player = function (filename) {
     this.plfile = filename;
     this.current = "";
     this.started = Date.now();
-    this.butTypes = ["play", "faster", "slower", "mute", "unmute", "next", "pause", "giantDoggo", "blur"];
-
+    this.rainbowing = false;
+    this.butTypes = ["play", "faster", "slower", "mute", "unmute", "next", "pause", "giantDoggo", "blur", "separate", "rain", "saturate"];
+    this.rainspawns = 0;
     this.buttonCount = 50;
     this.buttons = [];
     this.vid = document.querySelector("video");
@@ -23,7 +24,9 @@ Player.prototype.load = async function () {
         let time = this.currentTime;
 
         if (time > end) {
-            this.pause();
+            for (let v of document.querySelectorAll("video")) {
+                v.pause();
+            }
             playa.index++;
             console.log(playa.index, playa.length)
             playa.process();
@@ -37,7 +40,90 @@ Player.prototype.load = async function () {
 
     this.process();
     this.spawnButtons();
+
+    document.body.addEventListener("dblclick", () => {
+        this.separate();
+    });
+
+
 }
+
+
+Player.prototype.separate = async function (but) {
+
+
+    let playa = this;
+    let quikmojis = ["💔", "⛓️‍💥"]
+    but.textContent = quikmojis[rando(0, quikmojis.length - 1)]
+    but.addEventListener("click", async () => {
+
+
+        if (this.split) {
+            return false;
+        }
+
+        this.split = true;
+        let cont1 = document.querySelector(".container");
+        let coords = cont1.getBoundingClientRect();
+        let cw = coords.width;
+        let ch = coords.height;
+
+        let vid1 = this.vid;
+
+        let cont2 = cont1.cloneNode(true);
+
+        document.body.appendChild(cont2);
+
+        let vid2 = cont2.querySelector("video");
+
+        vid2.currentTime = vid1.currentTime;
+
+        if (!vid1.paused) {
+            vid2.play();
+        }
+
+        cont1.style.width = cw + "px";
+        cont1.style.height = ch + "px";
+        vid1.style.height = ch + "px";
+
+        cont2.style.width = cw + "px";
+        cont2.style.height = ch + "px";
+        vid2.style.height = ch + "px";
+
+        cont1.style.position = "absolute";
+        cont1.style.top = coords.y + "px";
+        cont1.style.left = coords.x + "px";
+
+        cont2.style.position = "absolute";
+        cont2.style.top = coords.y + "px";
+        cont2.style.right = coords.x + "px";
+
+        cont1.style.display = "block";
+        cont2.style.display = "block";
+
+        vid1.classList.remove("regVid");
+        vid2.classList.remove("regVid");
+        vid1.classList.add("splitVid");
+        vid2.classList.add("splitVid");
+
+        cont1.style.overflow = "hidden";
+        cont2.style.overflow = "hidden";
+
+        cont1.style.width = cw / 2 + "px";
+        cont2.style.width = cw / 2 + "px";
+        vid1.style.left = "0";
+        vid2.style.right = "100%";
+
+        await sleep(2500);
+
+        cont1.style.left = coords.x - 400 + "px";
+        cont2.style.right = coords.x - 400 + "px";
+
+    });
+
+}
+
+
 
 Player.prototype.spawnButtons = async function () {
 
@@ -49,6 +135,26 @@ Player.prototype.spawnButtons = async function () {
 }
 
 
+
+Player.prototype.rain = function (but) {
+    let playa = this;
+    console.log("rainrainrain");
+    let quikmojis = ["🌧️", "⛈️"];
+    but.classList.add("rain");
+    but.textContent = quikmojis[rando(0, quikmojis.length - 1)];
+    but.addEventListener("click", function () {
+        but.style.opacity = 0;
+
+    });
+    but.addEventListener("transitionend", function (e) {
+        if (e.propertyName == "opacity") {
+            let i = playa.buttons.indexOf(but);
+            playa.buttons.splice(i, 1);
+            but.remove();
+        }
+    }, { once: true });
+
+}
 
 
 Player.prototype.next = function (but) {
@@ -63,6 +169,24 @@ Player.prototype.next = function (but) {
 }
 
 
+
+Player.prototype.sat = function (but) {
+    let playa = this;
+    let quikmojis = ["🚥", "🚦"]
+    but.textContent = quikmojis[rando(0, quikmojis.length - 1)]
+    but.addEventListener("click", function () {
+        full.classList.toggle("saturated");
+    })
+
+}
+
+
+Player.prototype.rainbows = function () {
+    this.rainbowing = true;
+
+}
+
+
 Player.prototype.spdy = function (but) {
     let playa = this;
     let quikmojis = ["🐇", "🏃🏻‍♀️", "🏃🏻‍♂️", "🏃🏻‍➡️"]
@@ -73,12 +197,23 @@ Player.prototype.spdy = function (but) {
 
 }
 
+Player.prototype.rainbow = function (but) {
+    let playa = this;
+    let quikmojis = ["🌈", "🏳️‍🌈"];
+    but.classList.add("rainbow");
+    but.textContent = quikmojis[rando(0, quikmojis.length - 1)]
+
+
+}
+
 Player.prototype.paws = function (but) {
     let playa = this;
     let quikmojis = ["🐾"]
     but.textContent = quikmojis[rando(0, quikmojis.length - 1)]
     but.addEventListener("click", function () {
-        playa.vid.pause();
+        for (let v of document.querySelectorAll("video")) {
+            v.pause();
+        }
     });
 
 }
@@ -89,7 +224,9 @@ Player.prototype.otter = function (but) {
     let quikmojis = ["🦦"]
     but.textContent = quikmojis[rando(0, quikmojis.length - 1)]
     but.addEventListener("click", function () {
-        playa.vid.play();
+        for (let v of document.querySelectorAll("video")) {
+            v.play();
+        }
     });
 
 }
@@ -107,7 +244,7 @@ Player.prototype.rosie = function (but) {
         img.style.top = -400;
 
         img.style.left = -800;
-        img.style.zIndex = 9000;
+        img.style.zIndex = 999;
         document.body.appendChild(img);
 
 
@@ -122,7 +259,7 @@ Player.prototype.loud = function (but) {
     but.textContent = emoji[rando(0, emoji.length - 1)]
     but.addEventListener("click", function () {
         playa.vid.muted = false;
-        barqs.muted = false;    
+        barqs.muted = false;
         barqs.currentTime = 0;
         barqs.play();
     })
@@ -171,7 +308,23 @@ Player.prototype.slowpoke = function (but) {
 
 Player.prototype.spawnButton = async function () {
     let playa = this;
-    let type = this.butTypes[rando(0, this.butTypes.length - 1)];
+    let type;
+
+    if (this.rainspawns >= 50) {
+        this.rainbowing = false;
+        this.rainspawns = 0;
+    }
+
+    if (this.rainbowing && this.rainspawns < 50) {
+        type = "rainbow";
+        this.rainspawns++;
+    } else {
+        type = this.butTypes[rando(0, this.butTypes.length - 1)];
+    }
+
+
+
+
     let but = document.createElement("button");
     if (this.everythingBlurred) {
         but.classList.add("blursed");
@@ -195,6 +348,14 @@ Player.prototype.spawnButton = async function () {
         this.next(but);
     } else if (type === "blur") {
         this.blursed(but);
+    } else if (type === "separate") {
+        this.separate(but);
+    } else if (type === "rain") {
+        this.rain(but);
+    } else if (type === "rainbow") {
+        this.rainbow(but);
+    } else if (type === "saturate"){
+        this.sat(but);
     }
 
     else {
@@ -203,24 +364,32 @@ Player.prototype.spawnButton = async function () {
 
     but.style.left = rando(0, window.innerWidth) + "px";
     but.style.zIndex = rando(0, 100);
+    but.setAttribute("title", type)
     document.body.append(but);
     await sleep(rando(3, 5793));
 
-    but.addEventListener("transitionend", function () {
-        let rect = but.getBoundingClientRect();
+    but.addEventListener("transitionend", function (e) {
+        if (e.propertyName == "top") {
+            let rect = but.getBoundingClientRect();
 
-        if (rect.top >= window.innerHeight) {
-            playa.spawnButton();
-            but.remove();
-            let i = playa.buttons.indexOf(but);
-            playa.buttons.splice(i, 1);
+            if (rect.top >= window.innerHeight) {
+                if (but.classList.contains("rain")) {
+                    console.log("time for rainbows");
+                    playa.rainbowing = true;
+                }
+
+                playa.spawnButton();
+                but.remove();
+                let i = playa.buttons.indexOf(but);
+                playa.buttons.splice(i, 1);
+            }
+
+
         }
-
-
     })
 
 
-    but.style.top = window.innerHeight + "px";
+    but.style.top = "120vh";
     this.buttons.push(but);
 
 }
@@ -233,8 +402,9 @@ Player.prototype.process = function () {
         playa.index = 0;
     }
     this.current = this.videos[this.index];
-    this.vid.src = this.current.url;
-
+    for (let v of document.querySelectorAll("video")) {
+        v.src = this.current.url;
+    }
     console.log(this.current);
 
     if (this.current.start) {
@@ -260,8 +430,11 @@ Player.prototype.process = function () {
     }
 
     this.vid.addEventListener("loadedmetadata", function () {
-        this.currentTime = playa.current.start;
-        this.play();
+        for (let v of document.querySelectorAll("video")) {
+            v.currentTime = playa.current.start;
+            v.play();
+
+        }
     }, { once: true });
 
 
